4a752ebb2a5adf0bb135c48194e15a75e59844ab
Eric Pedley <ericpedley@gmail.com>
2024-03-04 18:32:27 -0800

    add docker manylinux env to actions and more distros to platforms line

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
     author_email="ericpedley@gmail.com",
     long_description="Python wrapper for the ZWO EAF Focuser C++ SDK",
     version="1.0",
-    platforms=["manylinux1_x86_64"]
+    platforms=["manylinux1_x86_64", "manylinux2010_x86_64", "manylinux2014_x86_64"]
 )
