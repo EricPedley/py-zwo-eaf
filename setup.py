ac4b938e422c58b5e83027bb7d4cdae9dd95f4a2
Eric Pedley <ericpedley@gmail.com>
2024-04-22 15:37:33 -0700

    rename sdk folder and add camera shared libraries

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 extensions = [
-    Extension("zwo_eaf",
+    Extension(
+        "zwo_eaf",
         ["src/zwo_eaf.pyx"],
         libraries=["EAFFocuser", "udev"],
-        library_dirs=["src/EAF_linux_mac_SDK_V1.6/lib/x64/", "/usr/lib"],
-        include_dirs=["src/EAF_linux_mac_SDK_V1.6/include"],
-        extra_link_args=["-Wl,-rpath,src/EAF_linux_mac_SDK_V1.6/lib/x64,-rpath,/usr/lib"],
+        library_dirs=["src/sdk/lib/x64/", "/usr/lib"],
+        include_dirs=["src/sdk/include"],
+        extra_compile_args=['-static'],
+        extra_link_args=[
+            "-Wl,-rpath,/usr/lib,-rpath,src/sdk/lib/x64"
+            ],
     )
 ]
 
