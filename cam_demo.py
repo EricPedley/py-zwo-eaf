3451fbb7b0b5281ef2cc618edffe9c60044afa96
Eric Pedley <ericpedley@gmail.com>
2024-10-22 15:05:03 -0700

    change settings in cam demo

--- cam_demo.py
+++ cam_demo.py
@@ -5,11 +5,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
-    cam = ASICamera(0, 0, 70)
+    cam = ASICamera(0, 200, 1000*1/60)
     for _ in tqdm(range(100000)):
         frame_color = cam.get_frame()
-
-        
         cv.imshow("frame", frame_color)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
